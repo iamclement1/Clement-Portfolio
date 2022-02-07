70e23101425a188b55b229608caedd186cdd063b
Nnamdi Clement <blessedmarcel1@gmail.com>
2022-02-07 13:05:30 +0100

    added navbar

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+    <div>
+        <div className='absolute'>
+            <div>Home Content</div>
+            <div>Clement img</div>
+        </div>
+    </div>
+    );
+};
+
+export default Home;
