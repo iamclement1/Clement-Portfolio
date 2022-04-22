09ad271234d4462279dad9a7b6c67c0b057d9577
Nnamdi Clement <blessedmarcel1@gmail.com>
2022-04-22 21:20:53 +0100

    added more project card

--- src/Components/Projects/ProjectCards.jsx
+++ src/Components/Projects/ProjectCards.jsx
@@ -1,6 +1,9 @@
 import Logistics from '../../assets/Logistics.png'
 import Fashion from '../../assets/Fashion.png'
 import Landing from '../../assets/Modern-Landing.png'
+import Urbane from '../../assets/Urbane.png'
+import Thestore from '../../assets/Thestore.png'
+import Easybank from '../../assets/Easybank.png'
 
 function ProjectCards() {
   return (
@@ -83,17 +86,16 @@ function ProjectCards() {
             md:space-y-0 space-y-3'>
                 <div className='md:max-w-sm min-w-screen rounded overflow-hidden
                 transform  transition duration-500 hover:scale-110 cursor-pointer'>
-                    <img src={Logistics} alt="" className='w-full' />
+                    <img src={Urbane} alt="" className='w-full' />
                     <div className='px-6 py-4 bg-[#191919] text-white'>
                         <div className='font-bold md:text-xl text-xs mb-2'>
-                            Logistics Page
+                            Urbane Foods
                         </div>
                         <p className='text-sm group-hover:text-white'>
-                            A Landing page that displays goods and makes product delivery services
-                            easy for clients. 
+                            A Restaurant Website that allows customers to order foods at their convienience.
                         </p>
 
-                        <a href="https://crystals-logistics.netlify.app/"
+                        <a href="https://urbane-foods.netlify.app/"
                         target="_blank">
                         <button className='bg-gradient-to-r from-pink-400 via-purple-500 
                             to-purple-600 text-white md:py-2 md:px-4 mb-3 mt-4 py-3 p-3 text-sm
@@ -106,17 +108,16 @@ function ProjectCards() {
                 </div>
                 <div className='md:max-w-sm min-w-screen rounded overflow-hidden
                 transform transition duration-500 hover:scale-110 cursor-pointer'>
-                    <img src={ Fashion } alt="" className='w-full' />
+                    <img src={ Easybank } alt="" className='w-full' />
                     <div className='px-6 py-4 bg-[#191919] text-white'>
                         <div className='font-bold md:text-xl text-xs mb-2'>
-                            Chungi Fashion 
+                            Easy Bank
                         </div>
                         <p className='text-sm group-hover:text-white'>
-                            A Fashion Blog Landing page developed for fashion freaks to get
-                            fashion updates. Page owned by Chungi Fashion.
+                            A Bank Landing Page given as a task from the freecodecamp community.
                         </p>
 
-                        <a href="https://alist-fashion.netlify.app/" target = '_blank'>
+                        <a href="https://the-easybank.netlify.app/" target = '_blank'>
                         <button className='bg-gradient-to-r from-pink-400 via-purple-500 
                             to-purple-600 text-white md:py-2 md:px-4 mb-3 mt-4 py-3 p-3 
                             text-sm rounded-md' type='btn'>
@@ -129,17 +130,16 @@ function ProjectCards() {
 
                 <div className='md:max-w-sm min-w-screen rounded overflow-hidden
                 transform  transition duration-500 hover:scale-110 cursor-pointer'>
-                    <img src={ Landing } alt="" className='w-full' />
+                    <img src={ Thestore } alt="" className='w-full' />
                     <div className='px-6 py-4 bg-[#191919] text-white '>
                         <div className='font-bold md:text-xl text-xs mb-2'>
-                            Landing Page
+                            The Store
                         </div>
                         <p className='text-sm group-hover:text-white'>
-                            Modern Landing page given as a task from frontend Mentor, to
-                            proof my level of competence as a frontend Developer
+                            The Store is an e-commerce application that allows customers add and view product.
                         </p>
 
-                        <a href="https://modern-lp.netlify.app/"
+                        <a href="https://the-store-b3c99.web.app/login"
                         target='_blank'>
                         <button className='bg-gradient-to-r from-pink-400 via-purple-500 
                             to-purple-600 text-white md:py-2 md:px-4 mb-3 mt-4 py-3 p-3  text-sm
