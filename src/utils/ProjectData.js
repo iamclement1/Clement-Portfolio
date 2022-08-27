5a728f66ef18b9120adf549dfe78c549bbfd5582
Nnamdi Clement <blessedmarcel1@gmail.com>
2022-08-27 15:02:07 +0100

    feats: created project-data file

---
+++ src/utils/ProjectData.js
@@ -0,0 +1,23 @@
+export const ProjectData = [
+    {
+        id: 1,
+        title: "Hostelpaddy",
+        description: "Hostel paddy is a platform with the vision of connecting students with house owners/agents to solve their hostel accommodation problems."
+    },
+    {
+        id: 2,
+        title: "The Store",
+        description: "The Store is an e-commerce application that allows customers add and view product."
+
+    },
+    {
+        id: 3,
+        title: "SomtinSomtin",
+        description: "Give your staff the power to choose how they send their Voucher. Simply allocate them an amount, and give them freedom to spend on any voucher."
+    },
+    {
+        id: 4,
+        title: "Urbane Foods",
+        description: "A Restaurant Website that allows customers to order foods at their convienience."
+    }
+]
