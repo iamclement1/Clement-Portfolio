95f3720ffff782a11c0c4459e0e14aaca7476ab4
Nnamdi Clement <blessedmarcel1@gmail.com>
2022-09-26 10:16:02 +0100

    feats: updated resume

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -51,7 +51,7 @@ const Home = () => {
                             rounded-md' type='btn'>Get in touch</button>
                             </NavLink>
 
-                            <a href="https://drive.google.com/file/d/1eFNXPuTptw2CMu9AhuPyqlR_GgEaT1re/view?usp=sharing" target="_blank">
+                            <a href="https://drive.google.com/file/d/1oXPh2HcyHSqCjsZbHFXJjL4vDjcYwWoL/view?usp=sharing" target="_blank">
                                 <button className='bg-transparent outline outline-gradient-to-r from outline-pink-300 via-purple-500 to-purple-600 text-white
                             md:py-5  md:px-8 px-3 py-4 text-sm rounded-sm ' type='btn'>
                                     View Resume
