d0580d3e32d7bdc98510c2c0103b98cbfede3868
iamclement1 <blessedmarcel1@gmail.com>
2022-10-15 07:50:25 +0100

    feats: updated footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
         <div className='md:relative block md:pt-0 pt-12 md:p-0 p-4 bg-[#0f0f11] 
         text-white text-xs md:text-xs md:w-5/7 w-full md:pl-16'>
             <div className='w-full text-xs text-white'>
-                <p>Copyright &copy; 2021 Clement | Designed by Phantom </p>
+                <p>Copyright &copy; 2021 Nnamdi Azubuike | Designed by Phantom </p>
             </div>
         </div>
     </div>
